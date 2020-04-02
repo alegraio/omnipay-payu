1d266f3544c64f2a2764d13a756a38509596c4ce
Can Sözeri <can.sozeri@alegradigital.com>
2020-04-02 16:47:07 +0300

    Add ItemBag and methods

--- src/PayUItem.php
+++ src/PayUItem.php
@@ -4,7 +4,16 @@ namespace Omnipay\PayU;
 
 use Omnipay\Common\Item;
 
-class PayUItem extends Item
+class PayUItem extends Item implements PayUItemInterface
 {
+    public function getSku()
+    {
+        return $this->getParameter('sku');
+    }
+
+    public function getPriceType()
+    {
+        return $this->getParameter('price_type') ?? 'with_vat';
+    }
 
 }
---
+++ src/PayUItemInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Omnipay\PayU;
+
+interface PayUItemInterface
+{
+    public function getSku();
+
+    public function getPriceType();
+
+}
